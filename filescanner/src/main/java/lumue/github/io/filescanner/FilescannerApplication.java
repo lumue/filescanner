63d9e6298c0a76d9c225b1b6095bf2734c9fb7d7
lm <mueller.lutz@gmail.com>
2015-03-05 11:37:18 +0100

    initial import

---
+++ filescanner/src/main/java/lumue/github/io/filescanner/FilescannerApplication.java
@@ -0,0 +1,12 @@
+package lumue.github.io.filescanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilescannerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FilescannerApplication.class, args);
+    }
+}
