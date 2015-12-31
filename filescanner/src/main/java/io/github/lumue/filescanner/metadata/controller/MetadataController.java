b94e64b62c4b2d252b605224c6bff29e1cdc0529
lm <mueller.lutz@gmail.com>
2015-12-31 06:15:28 +0100

    add controller method for document querying

--- filescanner/src/main/java/io/github/lumue/filescanner/metadata/controller/MetadataController.java
+++ filescanner/src/main/java/io/github/lumue/filescanner/metadata/controller/MetadataController.java
@@ -1,14 +1,17 @@
 package io.github.lumue.filescanner.metadata.controller;
 
+import com.google.common.collect.Lists;
 import io.github.lumue.filescanner.metadata.core.DocumentMetadata;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import io.github.lumue.filescanner.metadata.repository.MetadataRepository;
 
+import java.util.List;
+
 @RestController
+@RequestMapping("/documents")
+@CrossOrigin
 public class MetadataController {
 
 	private final MetadataRepository metadataRepository;
@@ -19,9 +22,13 @@ public class MetadataController {
 		this.metadataRepository = metadataRepository;
 	}
 
-	@RequestMapping("/metadata/find")
-	public DocumentMetadata find(@RequestParam String url) {
+	@RequestMapping("/{url}")
+	public DocumentMetadata find(@PathVariable("url") String url) {
 		return metadataRepository.findOne(url);
 	}
 
+	@RequestMapping(method=RequestMethod.GET)
+	public List<DocumentMetadata> list() {
+		return Lists.newArrayList(metadataRepository.findAll());
+	}
 }
